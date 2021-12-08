7ff802ca5719ef6cfa9250e3ac15834a7ffcc137
Jan Hlaváč <jan.hlavac@protonmail.com>
2021-12-08 15:09:57 +0100

    Added first part solution for day 8

---
+++ day-08/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_08
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            Console.WriteLine(FirstPart(input));
+            Console.WriteLine(SecondPart(input));
+        }
+
+        public static int FirstPart(string[] input)
+        {
+            string[][] entries = new string[input.Length][];
+            for (int i = 0; i < input.Length; i++)
+            {
+                string outputValues = input[i].Substring(input[i].LastIndexOf('|'));
+                entries[i] = (outputValues.Split(new Char[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries)).ToArray();
+            }
+            int uniques = 0;
+            foreach (var entry in entries)
+            {
+                foreach (string pattern in entry)
+                {
+                    if (pattern.Length >= 2 && pattern.Length <= 4 || pattern.Length == 7) { uniques++; }
+                }
+            }
+            return uniques;
+        }
+
+        public static int SecondPart(string[] input)
+        {
+            return -1;
+        }
+    }
+}
