59eec1ab3d2ebbe05cbbfd9a56e5d582dfe31139
Jan Hlaváč <jan.hlavac@protonmail.com>
2021-12-13 15:39:48 +0100

    Added solutions for day 11

---
+++ day-11/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+
+namespace day_11
+{
+    class Program
+    {
+        static int flashes = 0;
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            Console.WriteLine(FirstPart(input, 100));
+            Console.WriteLine(SecondPart(input, 300));
+        }
+
+        public static int FirstPart(string[] input, int steps)
+        {
+            int[,] octopuses = new int[input.Length, input[0].Length];
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++) { octopuses[y, x] = int.Parse(input[y][x].ToString()); }
+            }
+
+            for (int i = 0; i < steps; i++)
+            {
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[0].Length; x++) { octopuses[y, x]++; }
+                }
+
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[0].Length; x++)
+                    {
+                        if (octopuses[y, x] > 9) { FlashAndIncreaseAdjacent(octopuses, y, x); }
+                    }
+                }
+
+            }
+            return flashes;
+        }
+
+        public static int SecondPart(string[] input, int steps)
+        {
+            int terminalStep = -1;
+            int[,] octopuses = new int[input.Length, input[0].Length];
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++) { octopuses[y, x] = int.Parse(input[y][x].ToString()); }
+            }
+
+            for (int i = 0; i < steps; i++)
+            {
+                if(AreSynchronized(octopuses) && terminalStep == -1)
+                {
+                    terminalStep = i;
+                    break;
+                }
+
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[0].Length; x++) { octopuses[y, x]++; }
+                }
+
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[0].Length; x++)
+                    {
+                        if (octopuses[y, x] > 9) { FlashAndIncreaseAdjacent(octopuses, y, x); }
+                    }
+                }
+
+            }
+            return terminalStep;
+        }
+
+        public static void FlashAndIncreaseAdjacent(int[,] octopuses, int y, int x)
+        {
+            flashes++;
+            octopuses[y, x] = 0;
+            for (int vertical = y - 1; vertical < y + 2; vertical++)
+            {
+                for (int horizontal = x - 1; horizontal < x + 2; horizontal++)
+                {
+                    if ((vertical >= 0 && vertical < octopuses.GetLength(0)) && (horizontal >= 0 && horizontal < octopuses.GetLength(1)))
+                    {
+                        if (octopuses[vertical, horizontal] != 0) { octopuses[vertical, horizontal]++; }
+                        if (octopuses[vertical, horizontal] > 9) { FlashAndIncreaseAdjacent(octopuses, vertical, horizontal); }
+                    }
+                }
+            }
+        }
+
+        public static bool AreSynchronized(int[,] octo)
+        {
+            if (octo[0, 0] == 0 && octo[0, 1] == 0 && octo[1, 0] == 0)
+            {
+                for (int y = 0; y < octo.GetLength(0); y++)
+                {
+                    for (int x = 0; x < octo.GetLength(1); x++)
+                    {
+                        if (octo[y, x] != 0) { return false; }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
