5942712e7cd6c23e81a6fa0bfba9d67b606f918b
Jan Hlaváč <jan.hlavac@protonmail.com>
2021-12-14 22:50:15 +0100

    Added second part for day 14

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -9,7 +9,7 @@ namespace day_14
     {
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines("input.txt");
+            string[] input = File.ReadAllLines("example.txt");
             Console.WriteLine(FirstPart(input));
             Console.WriteLine(SecondPart(input));
         }
@@ -30,7 +30,7 @@ namespace day_14
             }
 
             string polymer = input[0];
-            // Adding counts for base polymer
+            // Adding single element counts of base polymer
             foreach (var element in polymer)
             {
                 elementQuantity[element]++;
@@ -55,9 +55,56 @@ namespace day_14
             return elementQuantity.Values.Max() - elementQuantity.Values.Min();
         }
         
-        public static int SecondPart(string[] input)
+        public static long SecondPart(string[] input)
         {
-            return -1;
+            Dictionary<string, Tuple<string, string>> pairsFromRule = new();
+            Dictionary<string, long> pairs = new();
+            Dictionary<char, long> elementQuantity = new();
+
+            // Adding single element counts of base polymer
+            string polymer = input[0];
+            for (int i = 0; i < polymer.Length; i++)
+            {
+                if (!elementQuantity.ContainsKey(polymer[i])) {
+                    elementQuantity.Add(polymer[i], 1);
+                } else
+                {
+                    elementQuantity[polymer[i]]++;
+                }
+            }
+            // Create all possible pairs and all possible pairs based on pair insertion rules
+            for (int i = 2; i < input.Length; i++)
+            {
+                char element = input[i].Substring(input[i].Length - 1, 1).ToCharArray()[0];
+                pairs.Add(input[i].Substring(0, 2), 0);
+                pairsFromRule.Add(input[i].Substring(0, 2), new(input[i].Substring(0, 1) + element, element + input[i].Substring(1, 1)));
+            }
+            // Adding counts of pairs from base polymer
+            for (int i = 1; i < polymer.Length; i++)
+            {
+                pairs[polymer[i - 1].ToString() + polymer[i].ToString()]++;
+            }
+            // Polymerization
+            for (int step = 0; step < 40; step++)
+            {
+                var currentPairs = new Dictionary<string, long>(pairs);
+                foreach (var pair in pairs)
+                {
+                    if (pair.Value > 0)
+                    {
+                        var generates = pairsFromRule[pair.Key];
+                        currentPairs[generates.Item1] += pair.Value;
+                        currentPairs[generates.Item2] += pair.Value;
+
+                        var character = generates.Item1[1];
+                        if (elementQuantity.ContainsKey(character)) { elementQuantity[character] += pair.Value; }
+                        else { elementQuantity.Add(character, pair.Value); }
+                        currentPairs[pair.Key] -= pair.Value;
+                    }
+                }
+                pairs = currentPairs;
+            }
+            return elementQuantity.Values.Max() - elementQuantity.Values.Min();
         }
     }
 }
